ad2dffd28a031b2ed802efc43622878710ca3621
kubner <jakubelewilczynski@gmail.com>
2022-09-01 18:51:00 +0200

    commit

--- dwm.c
+++ dwm.c
@@ -1831,7 +1831,7 @@ setup(void)
 	if (!drw_fontset_create(drw, fonts, LENGTH(fonts)))
 		die("no fonts could be loaded.");
 	lrpad = drw->fonts->h;
-	bh = drw->fonts->h + 3;
+	bh = drw->fonts->h + 2;
 	updategeom();
 	/* init atoms */
 	utf8string = XInternAtom(dpy, "UTF8_STRING", False);
